45d7500721d8ee4a8c7dc20debf6d966699bac44
Ekaterina Vinogradova <kanadi2909@gmail.com>
2022-05-16 16:33:32 +0300

    inital

---
+++ lab2test/UnitTests_task2.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace lab22
+{
+    public class UnitTests_task2
+    {
+        [Test]
+        public void Subscriber1Login()
+        {
+            Subscriber1 subscriber = new Subscriber1("Dasha");
+            string login = subscriber.Login;
+
+            string res = "Dasha";
+
+            Assert.AreEqual(res, login);
+        }
+
+        [Test]
+        public void Subscribe2Login()
+        {
+            Subscriber2 subscriber = new Subscriber2("Dasha");
+            string login = subscriber.Login;
+
+            string res = "Dasha";
+
+            Assert.AreEqual(res, login);
+        }
+
+        [Test]
+        public void Subscriber3Login()
+        {
+            Subscriber3 subscriber = new Subscriber3("Dasha");
+            string login = subscriber.Login;
+
+            string res = "Dasha";
+
+            Assert.AreEqual(res, login);
+        }
+    }
+}
